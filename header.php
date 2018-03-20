dd8e6bc1f4df1f9f06684de2ea58bf97d82c901d
jay ryan tugawin <jayryantugawin@gmail.com>
2018-03-20 13:56:25 +0800

    Create header.php

---
+++ header.php
@@ -0,0 +1,9 @@
+<?php include 'server.php';?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Web Design Davao Logs</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body class="login">
